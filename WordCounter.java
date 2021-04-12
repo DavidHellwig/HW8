c0d99a94af0327bda35744d76e483d0c16fd0e64
IAMACSStudent <74218656+IAMACSStudent@users.noreply.github.com>
2021-04-12 17:21:40 -0600

    Add files via upload

--- WordCounter.java
+++ WordCounter.java
@@ -1,9 +1,21 @@
 import java.io.File;
+
 import java.io.FileNotFoundException;
+
 import java.util.Hashtable;
+
 import java.util.Scanner;
+
 public class WordCounter {
-    Hashtable<String,Integer> wordTable = new Hashtable<String,Integer>();
-    Scanner bookworm = new Scanner(System.in).useDelimiter("[^a-zA-Z]+");
-    
+    public static void main(String[] args) throws FileNotFoundException {
+        Hashtable<String,Integer> wordTable = new Hashtable<>();
+        //Access the Jobs.txt file for reading, this should be in the same folder as the WordCounter program
+        File book = new File("Jobs.txt");
+
+        Scanner bookworm = new Scanner(book).useDelimiter("[^a-zA-Z]+");
+    }
+
+
+
+
 }
