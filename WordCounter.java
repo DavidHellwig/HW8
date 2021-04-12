e13164c8f93a00bc3c8863e6023cb5979d684433
IAMACSStudent <74218656+IAMACSStudent@users.noreply.github.com>
2021-04-12 17:10:35 -0600

    Add files via upload

---
+++ WordCounter.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Hashtable;
+import java.util.Scanner;
+public class WordCounter {
+    Hashtable<String,Integer> wordTable = new Hashtable<String,Integer>();
+    Scanner bookworm = new Scanner(System.in).useDelimiter("[^a-zA-Z]+");
+    
+}
